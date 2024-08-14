fb071d1f8b488a6015186dd4471eb9c084eed403
Rayoliveir <160605874+Rayoliveir@users.noreply.github.com>
2024-08-14 10:21:17 -0300

    Add files via upload

---
+++ Project_Hello_Word/src/main/java/com/mycompany/project_house_one/Project_house_one.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_house_one;
+
+/**
+ *
+ * @author Marcelly
+ */
+public class Project_house_one {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
